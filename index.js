f73ce72e109553a1769067959d116cc79a79f065
Tobias Koppers <tobias.koppers@googlemail.com>
2015-06-16 20:38:28 +0200

    fixed bug with empty atrule

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ module.exports = postcss.plugin('postcss-modules-local-by-default', function (op
         } else if(!globalMode) {
           atrule.params = ":local(" + atrule.params + ")";
         }
-      } else {
+      } else if(atrule.nodes) {
         atrule.nodes.forEach(function(decl) {
           if(decl.type === "decl") {
             localizeDecl(decl, {
--- test.js
+++ test.js
@@ -358,6 +358,7 @@ var tests = [
     input: '.a { background: url(./image.png); }\n' +
       ':global .b { background: url(image.png); }\n' +
       '.c { background: url("./image.png"); }\n' +
+      '@font-face { src: url("./font.woff"); }\n' +
       '@-webkit-font-face { src: url("./font.woff"); }',
     options: {
       rewriteUrl: function(global, url) {
@@ -368,7 +369,13 @@ var tests = [
     expected: ':local(.a) { background: url((local\\)./image.png\\\"local\\\"); }\n' +
       '.b { background: url((global\\)image.png\\\"global\\\"); }\n' +
       ':local(.c) { background: url(\"(local)./image.png\\\"local\\\"\"); }\n' +
+      '@font-face { src: url(\"(local)./font.woff\\\"local\\\"\"); }\n' +
       '@-webkit-font-face { src: url(\"(local)./font.woff\\\"local\\\"\"); }'
+  },
+  {
+    should: 'not crash on atrule without nodes',
+    input: '@charset "utf-8";',
+    expected: '@charset "utf-8";'
   }
 
 ];
